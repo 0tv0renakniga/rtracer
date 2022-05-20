cd67938854c3a0a3b845c522b518c83e11e34b1a
scotty <rroge036@gmail.com>
2022-05-20 10:28:18 -0600

    add point class

---
+++ include/geometry/point.hh
@@ -0,0 +1,38 @@
+#ifndef __POINT__HH__
+#define __POINT__HH__
+#include <iostream>
+
+namespace geo{
+
+class point{
+
+public:
+    point(): x(0),y(0),z(0) {} //default constructor
+    point(const double x_ ,const double y_ ,const double z_): x(x_) ,y(y_), z(z_) {}//intilization constructor
+    point(const double v_): x(v_), y(v_) , z(v_) {}
+    ~point(){}
+
+    //getter member function of point
+    double getx() const {return x;}
+    double gety() const {return y;}
+    double getz() const {return z;}
+
+    //setter member function of point
+    void setx(const double x_){ x = x_; }
+    void sety(const double y_){ y = y_; }
+    void setz(const double z_){ z = z_; }
+
+    //operator overload.. tell how to use defined operator
+    friend std::ostream& operator<< (std::ostream& os, const point& p){
+
+        os << p.x<<", "<< p.y << ", " << p.z;
+        return os;
+    }
+
+private:
+    double x,y,z;
+
+};//class point
+
+}
+#endif 
--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,21 @@
 #include <iostream>
+#include "geometry/point.hh"
 int main(int argc,char* argv[]){
     std::cout<<"hello"<<std::endl;
+    
+    geo::point p1;
+    geo::point p2(1,2,3);
+    geo::point p3(4);
+
+
+    
+    p1.setx(2);
+    p1.sety(5);
+    p1.setz(7);
+
+    std::cout<<"point p1 = "<<p1<< std::endl;
+    
+    
+    
     return 0;
 }
